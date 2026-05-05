bf8e76051ebcb0a90bbd32de04a5cc3b19317504
Ismail <Ismailchaudry123@icloud.com>
2026-05-05 12:13:02 -0500

    Some tweaks

--- PhoneBook.py
+++ PhoneBook.py
@@ -1,9 +1,19 @@
-def insert_number(name, number):
-    phone_Dict = {
-    }
-    phone_Dict['name']= number
+import json
 
-name = input("Whats the persons name?")
-number = input("Whats the persons number?")
+phone_Dict = {}
+
+name = input("Whats the persons name? ")
+while type(name) != str :
+    name = input("Whats the persons name? ")
+
+number = input("Whats the persons number? ")
+while not number.isdigit():    
+    number = input("Whats the persons number? ")
+
+def insert_number(name, number):  
+    if name in phone_Dict:
+        phone_Dict[name].append(number)  
+    phone_Dict[name] = number
 
 insert_number(name,number)
+print(phone_Dict)
