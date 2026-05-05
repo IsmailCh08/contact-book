a2d27273b4df5c453f25e5ebaef666fc2938d981
Ismail <Ismailchaudry123@icloud.com>
2026-05-05 11:02:16 -0500

    new file

---
+++ PhoneBook.py
@@ -0,0 +1,9 @@
+def insert_number(name, number):
+    phone_Dict = {
+    }
+    phone_Dict['name']= number
+
+name = input("Whats the persons name?")
+number = input("Whats the persons number?")
+
+insert_number(name,number)
