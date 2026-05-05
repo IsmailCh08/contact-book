28f06bcb3cb51ed451e9a6a33f1b1fc6c4bd1160
Ismail <Ismailchaudry123@icloud.com>
2026-05-05 13:39:12 -0500

    fixed options

--- PhoneBook.py
+++ PhoneBook.py
@@ -5,23 +5,25 @@ phone_Dict = {}
 name = ""
 number = ""
 if os.path.exists('PhoneBook.json'):
-    with open('phonebook.json', 'r') as file:
+    with open('PhoneBook.json', 'r') as file:
         phone_Dict = json.load(file)
 else:
     phone_Dict = {}
 
-def lookup_number(name):
-    name = "Whose number do you want to find? "
+def lookup_number():
+    name = input("Whose number do you want to find? ")
     print(phone_Dict[name])
-
-
-def delete_number(name):
-    name = input("Whose name do you want to delete? ")
-    phone_Dict.pop(name)
-    with open('phonebook.json', 'w') as file:
+    with open('PhoneBook.json', 'w') as file:
         json.dump(phone_Dict, file)
 
-def insert_number(name, number):  
+
+def delete_number():
+    name = input("Whose name do you want to delete? ")
+    phone_Dict.pop(name)
+    with open('PhoneBook.json', 'w') as file:
+        json.dump(phone_Dict, file)
+
+def insert_number():  
     name = input("Whats the persons name? ")
     while type(name) != str :
         name = input("Whats the persons name? ")
@@ -30,14 +32,12 @@ def insert_number(name, number):
     while not number.isdigit():    
         number = input("Whats the persons number? ")
 
-    if name in phone_Dict:
-        phone_Dict[name].append(number)  
     phone_Dict[name] = number
-    with open('phonebook.json', 'w') as file: 
+    with open('PhoneBook.json', 'w') as file: 
         json.dump(phone_Dict, file)
 
 option = input("What would you like to do today? Enter 1: to insert a number, Enter 2: delete a number, Enter 3: to lookup a number: ")
-
+option = int(option)
 if option == 1:
     insert_number()
 elif option == 2:
