affb0cb62f46120c3d4dc316a57ca1d24a7fabfb
Ismail <Ismailchaudry123@icloud.com>
2026-05-05 12:47:23 -0500

    JSON permanence

--- PhoneBook.py
+++ PhoneBook.py
@@ -1,6 +1,13 @@
 import json
+import os
 
 phone_Dict = {}
+
+if os.path.exists('PhoneBook.json'):
+    with open('phonebook.json', 'r') as file:
+        phone_Dict = json.load(file)
+else:
+    phone_Dict = {}
 
 name = input("Whats the persons name? ")
 while type(name) != str :
@@ -14,6 +21,7 @@ def insert_number(name, number):
     if name in phone_Dict:
         phone_Dict[name].append(number)  
     phone_Dict[name] = number
-
+    with open('phonebook.json', 'w') as file: 
+        json.dump(phone_Dict, file)
 insert_number(name,number)
 print(phone_Dict)
